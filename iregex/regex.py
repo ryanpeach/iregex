c4b238530143162f0aaa14cec8db1c3a5a3158d2
Ryan Peach <ryan.peach@keysight.com>
2021-05-13 19:50:24 -0400

    All tests passing

--- iregex/regex.py
+++ iregex/regex.py
@@ -8,6 +8,8 @@ from typing import Any, List, Optional, Union
 
 from iregex.consts import ANY, NEWLINE, ONE_OR_MORE, OPTIONAL, WHITESPACE, ZERO_OR_MORE
 from iregex.exceptions import (
+    AlreadyCapturedException,
+    AlreadyRepeatingException,
     NonEmptyError,
     NotACharacterException,
     SetIntersectionError,
@@ -27,7 +29,7 @@ class Regex:
     .. doctest::
 
         >>> Regex()
-        Regex()
+        Regex(r"")
 
         >>> Regex("asdf")
         Regex(r"asdf")
@@ -91,6 +93,9 @@ class Regex:
             >>> "hello" | Regex('world')
             Regex(r"(?:hello|world)")
 
+            >>> Regex("fizz") | Regex('buzz') | Regex('fizzbuzz')
+            Regex(r"(?:fizz|buzz|fizzbuzz)")
+
         :raises NonEmptyError: If either contains named capture groups.
         """
         return self | regex
@@ -129,9 +134,9 @@ class Regex:
         out._data.append(WHITESPACE + ZERO_OR_MORE)
         return out
 
-    def newline(self) -> "Regex":
+    def newlines(self) -> "Regex":
         r"""
-        Allows newline after text.
+        Allows newlines after text.
 
         .. testsetup::
 
@@ -139,13 +144,19 @@ class Regex:
 
         .. doctest::
 
-            >>> Regex("hello").newline().literal("world")
+            >>> Regex("hello").newlines().literal("world")
             Regex(r"hello(?:\n|\r\n?)*world")
 
         """
         out = copy(self)
         out._data.append(NEWLINE + ZERO_OR_MORE)
         return out
+
+    @staticmethod
+    def _is_repeating(txt: Union[str, "Regex"]) -> bool:
+        """Tests if a regex string is repeating."""
+        txt_: str = str(txt) if isinstance(txt, Regex) else txt
+        return txt_[-1] in (r"?", r"*", r"+", r"}")
 
     def zero_or_more_repetitions(self) -> "Regex":
         """
@@ -166,7 +177,10 @@ class Regex:
             >>> Regex("hello world").zero_or_more_repetitions()
             Regex(r"(?:hello world)*")
 
+        :raises AlreadyRepeatingException: If this is already a repeating regex.
         """
+        if Regex._is_repeating(self):
+            raise AlreadyRepeatingException("{self} is already repeating.")
         out = self.make_non_capture_group()
         out._data.append(ZERO_OR_MORE)
         return out
@@ -190,7 +204,10 @@ class Regex:
             >>> Regex("hello world").one_or_more_repetitions()
             Regex(r"(?:hello world)+")
 
+        :raises AlreadyRepeatingException: If this is already a repeating regex.
         """
+        if Regex._is_repeating(self):
+            raise AlreadyRepeatingException("{self} is already repeating.")
         out = self.make_non_capture_group()
         out._data.append(ONE_OR_MORE)
         return out
@@ -219,7 +236,10 @@ class Regex:
 
         :param m: At least this many times.
         :param n: At most this many times (inclusive).
+        :raises AlreadyRepeatingException: If this is already a repeating regex.
         """
+        if Regex._is_repeating(self):
+            raise AlreadyRepeatingException("{self} is already repeating.")
         out = self.make_non_capture_group()
         if m == 0 and n == 1:
             out._data.append(OPTIONAL)
@@ -247,7 +267,10 @@ class Regex:
             Regex(r"(?:hello world){3}")
 
         :param m: Exactly this many instances.
+        :raises AlreadyRepeatingException: If this is already a repeating regex.
         """
+        if Regex._is_repeating(self):
+            raise AlreadyRepeatingException("{self} is already repeating.")
         out = self.make_non_capture_group()
         out._data.append("{" + str(m) + "}")
         return out
@@ -269,24 +292,29 @@ class Regex:
             Regex(r"a+")
 
             >>> Regex("a").m_or_more_repetitions(3)
-            Regex(r"a{2}a+")
+            Regex(r"a{3,}")
 
             >>> Regex("[asdf]").m_or_more_repetitions(3)
-            Regex(r"[asdf]{2}[asdf]+")
+            Regex(r"[asdf]{3,}")
 
             >>> Regex("hello world").m_or_more_repetitions(3)
-            Regex(r"(?:hello world){2}(?:hello world)+")
+            Regex(r"(?:hello world){3,}")
 
         :param m: This many or more instances.
         :raises ValueError: When m < 0
+        :raises AlreadyRepeatingException: If this is already a repeating regex.
         """
+        if Regex._is_repeating(self):
+            raise AlreadyRepeatingException("{self} is already repeating.")
         if m == 0:
             return self.zero_or_more_repetitions()
         elif m == 1:
             return self.one_or_more_repetitions()
         elif m < 0:
             raise ValueError(f"m must be >= 0, got {m}")
-        return self.exactly_m_repetitions(m - 1) + self.one_or_more_repetitions()
+        out = self.make_non_capture_group()
+        out._data.append("{" + str(m) + ",}")
+        return out
 
     def optional(self) -> "Regex":
         """
@@ -307,12 +335,25 @@ class Regex:
             >>> Regex("hello world").optional()
             Regex(r"(?:hello world)?")
 
+        :raises AlreadyRepeatingException: If this is already a repeating regex.
         """
+        if Regex._is_repeating(self):
+            raise AlreadyRepeatingException("{self} is already repeating.")
         out = self.make_non_capture_group()
         out._data.append(OPTIONAL)
         return out
 
-    def any_char(self, *char: str) -> "Regex":
+    @staticmethod
+    def _is_character(txt: Union[str, "Regex"]) -> bool:
+        """Tests if a regex string is just a single character."""
+        txt_: str = str(txt) if isinstance(txt, Regex) else txt
+        if len(txt_) == 1:
+            return True
+        if len(txt_) == 2 and txt_[0] == "\\":
+            return True
+        return False
+
+    def any_char(self, *char: Union["Regex", str]) -> "Regex":
         """
         Any of the characters listed as parameters may be used.
 
@@ -336,14 +377,14 @@ class Regex:
         out = copy(self)
         for t in char:
             if not Regex._is_character(t):
-                raise NotACharacterException(f"{t} is not a character.")
+                raise NotACharacterException(fr"{t} is not a character.")
         if len(char) > 1:
-            out._data += ["["] + list(char) + ["]"]
+            out._data += ["["] + [str(c) for c in char] + ["]"]
         else:
-            out._data = list(char)
+            out._data = [str(c) for c in char]
         return out
 
-    def exclude_char(self, *char: str) -> "Regex":
+    def exclude_char(self, *char: Union["Regex", str]) -> "Regex":
         """
         None of the characters listed as parameters may be used.
 
@@ -364,9 +405,45 @@ class Regex:
         out = copy(self)
         for t in char:
             if not Regex._is_character(t):
-                raise NotACharacterException(f"{t} is not a character.")
-        out._data += ["[^"] + list(char) + ["]"]
+                raise NotACharacterException(fr"{t} is not a character.")
+        out._data += ["[^"] + [str(c) for c in char] + ["]"]
         return out
+
+    @staticmethod
+    def _is_character_group(txt: Union[str, "Regex"]) -> bool:
+        """Tests if a regex string is a character group."""
+        txt_: str = str(txt) if isinstance(txt, Regex) else txt
+        if len(txt_) >= 2 and txt_[0] == r"[" and txt_[-1] == r"]":
+            return True
+        return False
+
+    @staticmethod
+    def _is_named_capture_group(txt: Union[str, "Regex"]) -> bool:
+        """Tests if a regex string is a named capture group."""
+        txt_: str = str(txt) if isinstance(txt, Regex) else txt
+        if len(txt_) >= 5 and txt_[0:3] == r"(?<" and txt_[-1] == r")":
+            return True
+        return False
+
+    @staticmethod
+    def _is_non_capture_group(txt: Union[str, "Regex"]) -> bool:
+        """Tests if a regex string is a non capture group."""
+        txt_: str = str(txt) if isinstance(txt, Regex) else txt
+        if not Regex._is_named_capture_group(txt):
+            if len(txt_) >= 4 and txt_[0:3] == r"(?:" and txt_[-1] == r")":
+                return True
+        return False
+
+    @staticmethod
+    def _is_capture_group(txt: Union[str, "Regex"]) -> bool:
+        """Tests if a regex string is a capture group."""
+        txt_: str = str(txt) if isinstance(txt, Regex) else txt
+        if not Regex._is_named_capture_group(txt) and not Regex._is_non_capture_group(
+            txt
+        ):
+            if len(txt_) >= 2 and txt_[0] == r"(" and txt_[-1] == r")":
+                return True
+        return False
 
     def make_capture_group(self) -> "Regex":
         """
@@ -381,36 +458,24 @@ class Regex:
             >>> Regex(".*").make_capture_group().literal("world")
             Regex(r"(.*)world")
 
+            >>> Regex(".*").make_capture_group().make_capture_group().literal("world")
+            Regex(r"(.*)world")
+
+            >>> Regex(".*").make_non_capture_group().make_capture_group().literal("world")
+            Regex(r"(.*)world")
+
+        :raises AlreadyCapturedException: If this is already a named capture group.
         """
+        if Regex._is_named_capture_group(self):
+            raise AlreadyCapturedException(f"This is already a capture group: {self}")
+        if Regex._is_capture_group(self):
+            return self
         out = copy(self)
-        out._data = ["("] + out._data + [")"]
+        if Regex._is_non_capture_group(self):
+            out._data = ["(", str(out)[3:-1], ")"]
+        else:
+            out._data = ["("] + out._data + [")"]
         return out
-
-    @staticmethod
-    def _is_character(txt: Union[str, "Regex"]) -> bool:
-        """Tests if a regex string is just a single character."""
-        txt_: str = str(txt) if isinstance(txt, Regex) else txt
-        if len(txt_) == 1:
-            return True
-        if len(txt_) == 2 and txt_[0] == "\\":
-            return True
-        return False
-
-    @staticmethod
-    def _is_character_group(txt: Union[str, "Regex"]) -> bool:
-        """Tests if a regex string is a character group."""
-        txt_: str = str(txt) if isinstance(txt, Regex) else txt
-        if len(txt_) >= 2 and txt_[0] == r"[" and txt_[-1] == r"]":
-            return True
-        return False
-
-    @staticmethod
-    def _is_capture_group(txt: Union[str, "Regex"]) -> bool:
-        """Tests if a regex string is a capture group."""
-        txt_: str = str(txt) if isinstance(txt, Regex) else txt
-        if len(txt_) >= 2 and txt_[0] == r"(" and txt_[-1] == r")":
-            return True
-        return False
 
     def make_non_capture_group(self) -> "Regex":
         """
@@ -428,21 +493,26 @@ class Regex:
             >>> Regex("[asdf]").make_non_capture_group()  # This is unnecessary so it fails
             Regex(r"[asdf]")
 
-            >>> Regex("(hello world)").make_non_capture_group()  # This is unnecessary so it fails
-            Regex(r"(hello world)")
-
             >>> Regex("hello world").make_non_capture_group()  # This succeeds
             Regex(r"(?:hello world)")
 
+            >>> Regex("hello world").make_capture_group().make_non_capture_group()  # This succeeds
+            Regex(r"(?:hello world)")
+
+        :raises AlreadyCapturedException: If this is already a named capture group.
         """
         out = copy(self)
         # You don't need to make a non_capture_group for simple cases
         if (
             Regex._is_character(self)
             or Regex._is_character_group(self)
-            or Regex._is_capture_group(self)
+            or Regex._is_non_capture_group(self)
         ):
             out._data = out._data
+        elif Regex._is_named_capture_group(self):
+            raise AlreadyCapturedException(f"This is already a capture group: {self}")
+        elif Regex._is_capture_group(self):
+            out._data = ["(?:", str(out)[1:-1], ")"]
         else:
             out._data = ["(?:"] + out._data + [")"]
         return out
@@ -460,11 +530,25 @@ class Regex:
             >>> Regex(".*").make_named_capture_group("hello").literal("world")
             Regex(r"(?<hello>.*)world")
 
+            >>> Regex(".*").make_non_capture_group().make_named_capture_group("hello").literal("world")
+            Regex(r"(?<hello>.*)world")
+
+            >>> Regex(".*").make_capture_group().make_named_capture_group("hello").literal("world")
+            Regex(r"(?<hello>.*)world")
+
         :param name: The name to assign the capture group.
+        :raises AlreadyCapturedException: If this is already a named capture group.
         """
         out = copy(self)
-        out._data = ["(?<" + name + ">"] + out._data + [")"]
-        out._capture_groups.append(name)
+        if Regex._is_non_capture_group(self):
+            out._data = ["(?<", name, ">", str(out)[3:-1], ")"]
+        elif Regex._is_capture_group(self):
+            out._data = ["(?<", name, ">", str(out)[1:-1], ")"]
+        elif Regex._is_named_capture_group(self):
+            raise AlreadyCapturedException(f"{self} is already a named capture group.")
+        else:
+            out._data = ["(?<", name, ">"] + out._data + [")"]
+            out._capture_groups.append(name)
         return out
 
     def make_lookahead(self) -> "Regex":
@@ -650,7 +734,16 @@ class Regex:
             )
 
         out = Regex()
-        out._data = ["(?:"] + self._data + ["|"] + other_._data + [")"]
+        if Regex._is_non_capture_group(self):
+            out._data = ["(?:", str(self)[3:-1], "|"]
+            if Regex._is_non_capture_group(other_):
+                out._data += [str(other_)[3:-1], ")"]
+            else:
+                out._data += other_._data + [")"]
+        elif Regex._is_non_capture_group(other_):
+            out._data = ["(?:", str(self), "|", str(other_)[3:-1], ")"]
+        else:
+            out._data = ["(?:"] + self._data + ["|"] + other_._data + [")"]
         out._capture_groups = []
         return out
 
@@ -681,6 +774,15 @@ class Regex:
             )
 
         out = Regex()
-        out._data = ["(?:"] + other_._data + ["|"] + self._data + [")"]
+        if Regex._is_non_capture_group(other_):
+            out._data = ["(?:", str(other_)[3:-1], "|"]
+            if Regex._is_non_capture_group(self):
+                out._data += [str(self)[3:-1], ")"]
+            else:
+                out._data += self._data + [")"]
+        elif Regex._is_non_capture_group(self):
+            out._data = ["(?:", str(other_), "|", str(self)[3:-1], ")"]
+        else:
+            out._data = ["(?:"] + other_._data + ["|"] + self._data + [")"]
         out._capture_groups = []
         return out
