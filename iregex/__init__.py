0a5404fcb5f62cd3061bec70f85901522b550d8e
Ryan <ryanpeach@icloud.com>
2021-06-04 23:47:23 -0500

    Version 2.0

--- iregex/__init__.py
+++ iregex/__init__.py
@@ -5,4 +5,4 @@ Makes certain items top level imports.
 from iregex.consts import *  # noqa: F403, F401
 from iregex.regex import *  # noqa: F403, F401
 
-__version__ = "0.1.16"
+__version__ = "0.2.0"
